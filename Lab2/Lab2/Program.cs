f556babb672dfb5505ae82b4730e47283cfc2fad
GusevMaksim123 <156201572+GusevMaksim123@users.noreply.github.com>
2024-01-10 16:16:28 +0300

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Lab2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new WordsDictionary();
+            Console.WriteLine("Добро пожаловать в программу словаря однокоренных слов");
+            Console.WriteLine("Для завершения введите 'end'.");
+            while (true)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Чтобы начать, введите слово на кириллице:");
+                string message = Console.ReadLine();
+                if (message == "end")
+                {
+                    break;
+                }
+                session.Check(message);
+            }
+        }
+    }
+}
