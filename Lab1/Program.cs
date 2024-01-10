ee2b924f0b376388a1567b5efe8abdaeb9835f0e
GusevMaksim123 <156201572+GusevMaksim123@users.noreply.github.com>
2024-01-10 16:30:07 +0300

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,67 @@
+﻿using Lab1;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Часть 1:");
+            Console.Write("Введите размер массива: ");
+
+            int size = int.Parse(Console.ReadLine());
+
+            var firstPart = new FirstPart(size);
+
+            Console.WriteLine("Исходный массив: ");
+            PrintVector(firstPart.Vector);
+
+            Console.WriteLine("Минимальный элемент: ");
+            Console.WriteLine(firstPart.GetMin());
+
+            Console.WriteLine("Сумма элементов между первым и последним положительными: ");
+            Console.WriteLine(firstPart.GetSumBetweenPositive());
+
+            Console.WriteLine("Сортированный массив: ");
+            firstPart.SortByZero();
+            PrintVector(firstPart.Vector);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Часть 2:");
+            var secondPart = new SecondPart(10, 10);
+            Console.WriteLine("Исходный массив: ");
+            PrintMatrix(secondPart.Matrix);
+            Console.WriteLine(secondPart.GetElement(2,3));
+
+            Console.WriteLine("Сумма элементов строк с отрицательными элементами: ");
+            Console.WriteLine(secondPart.GetSumOfRowsWithBelowZero());
+
+            Console.WriteLine("Седловые точки матрицы: ");
+            secondPart.GetSaddlePoints();
+            
+            Console.WriteLine("Седловые точки матрицы, тест: ");
+            var test = new SecondPart();
+            PrintMatrix(test.Matrix);
+            test.GetSaddlePoints();
+        }
+
+
+        static void PrintVector(IEnumerable<int> vector)
+        {
+            Console.WriteLine(string.Join(" ", vector));
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0,4}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lab1/SecondPart.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class SecondPart
+    {
+        private readonly int[,] matrix;
+
+        public int GetElement(int rows, int cols)
+        {
+            int element = matrix[rows, cols];
+            return element;
+        }
+
+        public SecondPart()
+        {
+            matrix = new int[,]{
+                { 1, 2, 3 },
+                { -4, 5, -6 },
+                { 7, 8, 9 }};
+        }
+        public SecondPart(int rows, int cols)
+        {
+            if (rows < 0 || cols < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rows) + " or " + nameof(cols));
+            }
+
+            matrix = new int[rows, cols];
+
+            var random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(-10, 10);
+                }
+            }
+        }
+
+        public int[,] Matrix
+        {
+            get
+            {
+                return matrix;
+            }
+        }
+
+        public int GetSumOfRowsWithBelowZero()
+        {
+            int counterSum = 0;
+            int c = 0;
+            int mark = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    c += matrix[i, j];
+                    if (matrix[i, j] < 0)
+                    {
+                        mark = 1;
+                    }
+                }
+                if (mark == 1)
+                {
+                    counterSum += c;
+                }
+                c = 0;
+                mark = 0;
+            }
+
+            return counterSum;
+        }
+
+        public void GetSaddlePoints()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int minElement = int.MaxValue;
+                int columnOfMinElement = -1;
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int element = matrix[i, j];
+                    if (element < minElement)
+                    {
+                        minElement = element;
+                        columnOfMinElement = j;
+                    }
+                }
+                if (IsSaddlePoint(i, columnOfMinElement))
+                {
+                    Console.WriteLine("Седловая точка: [{0}, {1}]", i, columnOfMinElement);
+                }
+            }
+        }
+
+        private bool IsSaddlePoint(int rowIndex, int columnOfMinElement)
+        {
+            int element = matrix[rowIndex, columnOfMinElement];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, columnOfMinElement] > element)
+                {
+                    return false;
+                }
+            }
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[rowIndex, j] < element)
+                    {
+                    return false;
+                    }
+            }
+                return true;
+        }
+    }
+}
